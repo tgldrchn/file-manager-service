b236fffdb91be15dcaffa3167eb4817d08a8e755
tgldrchn <tuguldur9190@gmail.com>
2026-05-14 17:12:43 +0800

    Added docker file

--- src/main/java/com/example/demo/FileManagerServiceApplication.java
+++ src/main/java/com/example/demo/FileManagerServiceApplication.java
@@ -2,12 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example"}) 
 public class FileManagerServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(FileManagerServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FileManagerServiceApplication.class, args);
+    }
 }
